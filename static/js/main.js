1ff86c31ddcce73a9ed1193b5bdddd37f0705c0b
Naman82 <b520031@iiit-bh.ac.in>
2022-01-21 12:36:58 +0530

    connections made

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,16 @@ var usernameInput = document.querySelector('#username');
 var btnJoin = document.querySelector('#btn-join');
 
 var username;
+var webSocket;
+
+function webSocketOnMessage(event){
+
+    var parsedData=JSON.parse(event.data);
+    var message = parsedData['message'];
+
+    console.log('message :',message);
+
+}
 
 btnJoin.addEventListener('click',() => {
     username=usernameInput.value;
@@ -35,4 +45,18 @@ btnJoin.addEventListener('click',() => {
     var endPoint= wsStart+ loc.host+ loc.pathname;
 
     console.log('endPoint : ',endPoint);
+
+    webSocket=new WebSocket(endPoint);
+
+
+    webSocket.addEventListener('open',(e) => {
+        console.log('Connection open!');
+    });
+    webSocket.addEventListener('message',webSocketOnMessage);
+    webSocket.addEventListener('close',(e) => {
+        console.log('Connection closed!');
+    });
+    webSocket.addEventListener('error',(e) => {
+        console.log('Error Occurred!')
+    });
 });
