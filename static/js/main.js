9a07428caa900f312cd2e13c79a22d83ca9c2f29
Naman82 <b520031@iiit-bh.ac.in>
2022-01-21 21:33:05 +0530

    css updated

--- static/js/main.js
+++ static/js/main.js
@@ -10,9 +10,21 @@ var webSocket;
 function webSocketOnMessage(event){
 
     var parsedData=JSON.parse(event.data);
-    var message = parsedData['message'];
+    var peerUsername = parsedData['peer'];
+    var action = parsedData['action'];
 
-    console.log('message :',message);
+    if(username == peerUsername){
+        return;
+    }
+
+    // console.log('message : ',message);
+    var receiver_channel_name = parsedData['message']['receiver_channel_name'];
+
+    if(action == 'new-peer'){
+        createOfferer(peerUsername, receiver_channel_name);
+
+        return;
+    }
 
 }
 
@@ -51,12 +63,76 @@ btnJoin.addEventListener('click',() => {
 
     webSocket.addEventListener('open',(e) => {
         console.log('Connection open!');
+
+        sendSignal('new-peer',{});
+
+        webSocket.send(jsonStr);
+
     });
+
+
     webSocket.addEventListener('message',webSocketOnMessage);
+
+
     webSocket.addEventListener('close',(e) => {
         console.log('Connection closed!');
     });
+
+
     webSocket.addEventListener('error',(e) => {
         console.log('Error Occurred!')
     });
 });
+
+
+var localStream = new MediaStream();
+
+const constraints ={
+    'video':true,
+    'audio':true
+};
+
+const localVideo = document.querySelector('#local-video');
+
+var userMedia = navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+        localStream=stream;
+        localVideo.srcObject =localStream;
+        localVideo.muted =true;
+    })
+
+    .catch(error => {
+        console.log('Error accessing media devices.', error);
+    });
+
+    function sendSignal(action,message){
+        var jsonStr=JSON.stringify({
+            'peer':username,
+            'action':action,
+            'message':message,
+        });
+
+        webSocket.send(jsonStr);
+    }
+
+
+    function createOfferer(peerUsername, receiver_channel_name){
+        var peer = new RTCPeerConnection(null);
+
+        addLocalTracks(peer);
+
+        var dc =peer.createDataChannel('channel');
+        dc.addEventListener('open', () => {
+            console.log('Connection opened!');
+        });
+
+        
+    }
+
+    function addLocalTracks(peer){
+        localStream.getTracks().forEach(track => {
+            peer.addTrack(track, localStream);
+        });
+
+        return;
+    }
